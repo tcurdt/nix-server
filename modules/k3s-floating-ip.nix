334520bd326fd7cbf89e074ae4646cfc8e8650b1
Torsten Curdt <tcurdt@vafer.org>
2026-05-07 02:27:10 +0200

    replace instead of add

--- modules/k3s-floating-ip.nix
+++ modules/k3s-floating-ip.nix
@@ -52,16 +52,16 @@ in
           let
             ipv4Cmd = optional (
               cfg.ipv4 != null
-            ) "${pkgs.iproute2}/bin/ip addr add ${cfg.ipv4} dev ${cfg.interface}";
+            ) "${pkgs.iproute2}/bin/ip addr replace ${cfg.ipv4} dev ${cfg.interface}";
             ipv4RouteCmd = optional (
               cfg.ipv4Gateway != null
-            ) "${pkgs.iproute2}/bin/ip route add default via ${cfg.ipv4Gateway} dev ${cfg.interface}";
+            ) "${pkgs.iproute2}/bin/ip route replace default via ${cfg.ipv4Gateway} dev ${cfg.interface}";
             ipv6Cmd = optional (
               cfg.ipv6 != null
-            ) "${pkgs.iproute2}/bin/ip addr add ${cfg.ipv6} dev ${cfg.interface}";
+            ) "${pkgs.iproute2}/bin/ip -6 addr replace ${cfg.ipv6} dev ${cfg.interface}";
             ipv6RouteCmd = optional (
               cfg.ipv6Gateway != null
-            ) "${pkgs.iproute2}/bin/ip -6 route add default via ${cfg.ipv6Gateway} dev ${cfg.interface}";
+            ) "${pkgs.iproute2}/bin/ip -6 route replace default via ${cfg.ipv6Gateway} dev ${cfg.interface}";
           in
           ipv4Cmd ++ ipv4RouteCmd ++ ipv6Cmd ++ ipv6RouteCmd;
 
