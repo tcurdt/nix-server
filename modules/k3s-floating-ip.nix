14ce48ea71c077c77b00ebc335191f5e0e6dfa3a
Torsten Curdt <tcurdt@vafer.org>
2026-05-07 02:32:18 +0200

    no fail

--- modules/k3s-floating-ip.nix
+++ modules/k3s-floating-ip.nix
@@ -69,16 +69,16 @@ in
           let
             ipv4Cmd = optional (
               cfg.ipv4 != null
-            ) "${pkgs.iproute2}/bin/ip addr del ${cfg.ipv4} dev ${cfg.interface}";
+            ) "-${pkgs.iproute2}/bin/ip addr del ${cfg.ipv4} dev ${cfg.interface}";
             ipv4RouteCmd = optional (
               cfg.ipv4Gateway != null
-            ) "${pkgs.iproute2}/bin/ip route del default via ${cfg.ipv4Gateway} dev ${cfg.interface}";
+            ) "-${pkgs.iproute2}/bin/ip route del default via ${cfg.ipv4Gateway} dev ${cfg.interface}";
             ipv6Cmd = optional (
               cfg.ipv6 != null
-            ) "${pkgs.iproute2}/bin/ip addr del ${cfg.ipv6} dev ${cfg.interface}";
+            ) "-${pkgs.iproute2}/bin/ip -6 addr del ${cfg.ipv6} dev ${cfg.interface}";
             ipv6RouteCmd = optional (
               cfg.ipv6Gateway != null
-            ) "${pkgs.iproute2}/bin/ip -6 route del default via ${cfg.ipv6Gateway} dev ${cfg.interface}";
+            ) "-${pkgs.iproute2}/bin/ip -6 route del default via ${cfg.ipv6Gateway} dev ${cfg.interface}";
           in
           ipv4Cmd ++ ipv4RouteCmd ++ ipv6Cmd ++ ipv6RouteCmd;
       };
